976e805da112e8abdce035fbe7b38025a83b38b5
extrasharp <genericpb@gmail.com>
2020-08-09 23:53:38 -0500

    yay

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,219 @@
+use std::{
+    time::{
+        Instant,
+    },
+};
 
+use flume::{
+    self,
+    Sender,
+    Receiver,
+};
 
-type Sample = f32;
+//
 
-struct Event {
-    timestamp: Instant,
-    id: usize,
-    // path: Vec<String> a path
-}
+pub type Sample = f32;
 
-struct AudioContext {
+pub struct AudioContext {
     sample_rate: u32,
-    mixer: Mixer,
+    now: Instant,
 }
 
-trait Module: Send + Sync {
+impl AudioContext {
+    pub fn new() -> Self {
+        Self {
+            sample_rate: 44100,
+            now: Instant::now(),
+        }
+    }
+}
+
+pub trait Module: Send {
+    fn frame(&mut self, _ctx: &AudioContext) {}
     fn compute(&mut self,
                ctx: &AudioContext,
                in_buf: &[Sample; 2],
                out_buf: &mut [Sample; 2]);
-    fn handle_event(&mut self, _ctx: &AudioContext, _ev: &Event) {}
 }
 
 //
 
-struct Mixer {
-    tracks: Vec<Track>,
-    chains: Vec<ChainSend>,
-}
-
-struct Track {
-    volume: f32,
-    pan: f32,
-}
-
-struct ChainSend {
-    track_id: usize,
-    chain: Chain,
-}
-
-//
-
-struct Chain {
+pub struct Mixer {
     modules: Vec<Box<dyn Module>>,
 }
 
+impl Mixer {
+    pub fn new() -> Self {
+        Self {
+            modules: Vec::new(),
+        }
+    }
+
+    pub fn add_module(&mut self, module: Box<dyn Module>) {
+        self.modules.push(module);
+    }
+}
+
+impl Module for Mixer {
+    fn frame(&mut self, ctx: &AudioContext) {
+        for m in self.modules.iter_mut() {
+            m.frame(ctx);
+        }
+    }
+
+    fn compute(&mut self,
+               ctx: &AudioContext,
+               in_buf: &[Sample; 2],
+               out_buf: &mut [Sample; 2]) {
+        let mut out = [0.; 2];
+        for m in self.modules.iter_mut() {
+            let mut computed = [0.; 2];
+            m.compute(ctx, &in_buf, &mut computed);
+            out[0] += computed[0];
+            out[1] += computed[1];
+        }
+        *out_buf = out;
+    }
+}
+
+//
+
+pub struct Chain {
+    modules: Vec<Box<dyn Module>>,
+}
+
+impl Chain {
+    pub fn new() -> Self {
+        Self {
+            modules: Vec::new(),
+        }
+    }
+
+    pub fn add_module(&mut self, module: Box<dyn Module>) {
+        self.modules.push(module);
+    }
+}
+
 impl Module for Chain {
+    fn frame(&mut self, ctx: &AudioContext) {
+        for m in self.modules.iter_mut() {
+            m.frame(ctx);
+        }
+    }
+
     fn compute(&mut self,
                ctx: &AudioContext,
                in_buf: &[Sample; 2],
                out_buf: &mut [Sample; 2]) {
         let mut local_in_buf = *in_buf;
-        let mut local_out_buf = [0_f32; 2];
+        let mut local_out_buf = [0.; 2];
         for m in self.modules.iter_mut() {
             m.compute(ctx, &local_in_buf, &mut local_out_buf);
             local_in_buf = local_out_buf;
         }
         *out_buf = local_out_buf;
     }
-
-    /*
-    fn handle_event(&mut self, ctx: &AudioContext, ev: &Event) {
-        for m in self.modules.iter_mut() {
-            m.handle_event(ctx, ev);
-        }
-    }
-    */
 }
 
 //
 
-// instrument is shared by main thread and audio thread
-struct InstrumentModule {
-    inst: Arc<Mutex<Instrument>>,
+pub struct Event<T> {
+    pub timestamp: Instant,
+    pub data: T,
 }
 
-impl Module for InstrumentModule {
-    fn compute(&mut self,
-               ctx: &AudioContext,
-               in_buf: &[Sample; 2],
-               out_buf: &mut [Sample; 2]) {
+struct EventReciever<T> {
+    recv: Receiver<Event<T>>,
+    last_event: Option<Event<T>>,
+}
+
+//
+
+pub enum UtilityEvent {
+    Volume(f32),
+    Pan(f32),
+}
+
+pub struct Utility {
+    recv: Receiver<Event<UtilityEvent>>,
+    last_event: Option<Event<UtilityEvent>>,
+
+    // 0 to 1
+    volume: f32,
+
+    // -1 to 1
+    pan: f32,
+}
+
+impl Utility {
+    pub fn new() -> (Self, Sender<Event<UtilityEvent>>) {
+        let (tx, rx) = flume::unbounded();
+        let ret = Self {
+            recv: rx,
+            last_event: None,
+            volume: 1.,
+            pan: 0.,
+        };
+        (ret, tx)
+    }
+
+    fn do_event(&mut self, ev: UtilityEvent) {
+        match ev {
+            UtilityEvent::Volume(val) => self.volume = val,
+            UtilityEvent::Pan(val) => self.pan = val,
+        }
     }
 }
+
+impl Module for Utility {
+    fn frame(&mut self, _ctx: &AudioContext) {
+        let ev = self.recv.try_recv();
+        match ev {
+            Ok(ev) => {
+                self.do_event(ev.data);
+            },
+            Err(_) => {}
+        }
+    }
+
+    fn compute(&mut self,
+               _ctx: &AudioContext,
+               in_buf: &[Sample; 2],
+               out_buf: &mut [Sample; 2]) {
+        let pan = self.pan / 2. + 0.5;
+        out_buf[0] = in_buf[0] * self.volume * (1. - pan);
+        out_buf[1] = in_buf[1] * self.volume * pan;
+    }
+}
+
+//
+
+pub struct Sine {
+    frame_ct: u32,
+}
+
+impl Sine {
+    pub fn new() -> Self {
+        Self {
+            frame_ct: 0,
+        }
+    }
+}
+
+impl Module for Sine {
+    fn compute(&mut self,
+               _ctx: &AudioContext,
+               _in_buf: &[Sample; 2],
+               out_buf: &mut [Sample; 2]) {
+        out_buf[0] = f32::sin(self.frame_ct as f32 / 10.);
+        out_buf[1] = f32::sin(self.frame_ct as f32 / 10.);
+        self.frame_ct += 1;
+    }
+}
+
+//
+
+/*
 
 struct Instrument {
     bank: SampleBank,
@@ -106,3 +239,4 @@ struct FileStream {
 }
 
 // inst.add_sample()
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,71 @@
-fn main() {
-}
-
-/*
-use jack;
-use jack::Client;
-
-use std::time;
-use std::sync::{
-    mpsc,
-    Arc,
-    Mutex,
+use std::{
+    thread,
+    time::{
+        Duration,
+        Instant,
+    },
 };
-use flume;
 
-struct Chain {
-    modules: Vec<Box<dyn Module>>,
-    buf: [f32; 2],
-}
+use cpal::{
+    self,
+    Data,
+    SampleRate,
+    traits::*,
+};
 
-impl Chain {
-    fn compute(&mut self) {
-        let mut in_buf = [0_f32; 2];
-        let mut out_buf = [0_f32; 2];
-        for m in self.modules.iter_mut() {
-            m.compute(&in_buf, &mut out_buf);
-            in_buf = out_buf;
-        }
-        self.buf = out_buf;
-    }
-}
-
-trait Module: Send + Sync {
-    // Takes an AudioContext that has sample rate and stuff
-    fn compute(&mut self, in_buf: &[f32; 2], out_buf: &mut [f32; 2]);
-    fn handle_event(&self) {}
-}
-
-struct Sine {
-    frame_ct: f32,
-}
-
-impl Module for Sine {
-    fn compute(&mut self, _in_buf: &[f32; 2], out_buf: &mut [f32; 2]) {
-        out_buf[0] = f32::sin(self.frame_ct / 10_f32);
-        out_buf[1] = f32::sin(self.frame_ct / 10_f32);
-        self.frame_ct += 1.;
-    }
-}
-
-struct Distort;
-
-impl Module for Distort {
-    fn compute(&mut self, in_buf: &[f32; 2], out_buf: &mut [f32; 2]) {
-        fn dist(f: f32) -> f32 {
-            if f > 0_f32 {
-                1_f32
-            } else {
-                -1_f32
-            }
-        }
-
-        out_buf[0] = dist(in_buf[0]);
-        out_buf[1] = dist(in_buf[1]);
-    }
-}
+use kasumi::*;
 
 fn main() {
-    let c_res = Client::new("kasumi", jack::ClientOptions::NO_START_SERVER);
+    let host = cpal::default_host();
 
-    let (client, status) = match c_res {
-        Ok(res) => res,
-        Err(e) => panic!("Failed to open client because of error: {:?}", e),
-    };
+    let mut devices = host.devices().unwrap();
+    devices.for_each(|d| println!("{}", d.name().unwrap()));
 
-    let mut out_port = client.register_port("kasumi_out", jack::AudioOut::default())
-        .unwrap();
+    let mut devices = host.devices().unwrap();
+    let device = devices.find(| d | {
+        d.name().unwrap() == "pulse"
+    }).unwrap();
 
-    let mut ch = Chain {
-        modules: vec![Box::new(Sine { frame_ct: 0_f32})],
-        buf: [0f32; 2],
-    };
+    let mut supported_configs_ranges = device.supported_output_configs()
+        .expect("error while querying configs");
+    let supported_config_range = supported_configs_ranges.next()
+        .expect("no supported configs");
+    let s_conf = supported_config_range.with_sample_rate(SampleRate(44100));
+    let mut config = s_conf.config();
+    config.channels = 2;
 
-    let (tx, rx) = flume::unbounded::<f64>();
-    let (tx2, rx2) = flume::unbounded::<f64>();
-    // let (tx, rx) = mpsc::channel::<f64>();
-    // let (tx2, rx2) = mpsc::channel::<f64>();
-    let am_tx = Arc::new(Mutex::new(tx));
-    let am_tx2 = Arc::new(Mutex::new(tx2));
-    let tm = Arc::new(time::Instant::now());
+    let mut mixer = Mixer::new();
+    let mut chain = Chain::new();
+    let (util, tx) = Utility::new();
+    let sine = Sine::new();
 
-    let tm_c = Arc::clone(&tm);
-    let cb = move |_: &Client, ps: &jack::ProcessScope | -> jack::Control {
-        let out = out_port.as_mut_slice(ps);
-        let time = tm_c.elapsed().as_secs_f64();
-        am_tx.lock().unwrap().send(time);
-        let gua = am_tx2.lock().unwrap();
-        for _ in 0..1000 {
-            gua.send(time);
-        }
-        for v in out.iter_mut() {
-            ch.compute();
-            *v = ch.buf[0];
-        }
-        jack::Control::Continue
-    };
+    chain.add_module(Box::new(sine));
+    chain.add_module(Box::new(util));
+    mixer.add_module(Box::new(chain));
 
-    let proc = jack::ClosureProcessHandler::new(cb);
+    let ac = AudioContext::new();
 
-    let active_client = client.activate_async((), proc).unwrap();
+    let stream = device.build_output_stream(
+        &config,
+        move | data: &mut [f32], cbi: &cpal::OutputCallbackInfo | {
+            mixer.frame(&ac);
 
-    let mut last = 0.;
-    let mut max = -1.;
-    let mut ct = 0;
+            let dummy = [0.; 2];
+            for i in 0..(data.len()/2) {
+                let mut buf = [0.; 2];
+                mixer.compute(&ac, &dummy, &mut buf);
+                data[i * 2] = buf[0];
+                data[i * 2 + 1] = buf[1];
+            }
+        },
+        move |err| {
+        },
+    );
+
     loop {
-        let now = rx.recv().unwrap();
-        max = f64::max(max, now - last);
-        println!("{:1.8} {:1.8}", now - last, max);
-        last = now;
-        ct += 1;
-        if ct > 20 {
-            ct = 0;
-            max = -1.;
-        }
+        thread::sleep(Duration::from_secs(1));
+        tx.send(Event{timestamp: Instant::now(), data: UtilityEvent::Pan(1.)});
+        thread::sleep(Duration::from_secs(1));
+        tx.send(Event{timestamp: Instant::now(), data: UtilityEvent::Pan(-1.)});
     }
 }
-*/
